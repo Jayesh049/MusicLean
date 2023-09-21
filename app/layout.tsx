be796efbff94274506a45809ec2dd4e5f443d20c
Jayesh049 <jampotom421@gmail.com>
2023-09-21 13:23:50 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import './globals.css'
+import { Figtree } from 'next/font/google';
+
+const inter = Figtree({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Spotify Clone',
+  description: 'Listen to music!',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {children}
+        </body>
+    </html>
+  )
+}
